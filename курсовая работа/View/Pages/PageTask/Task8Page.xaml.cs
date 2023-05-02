d5f6db9ef4bb012fe2598aeafbb5de5d64f9ad38
user <user@DESKTOP-S25JFKU>
2023-05-02 20:57:05 +0300

    Добавьте файлы проекта.

---
+++ курсовая работа/View/Pages/PageTask/Task8Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace курсовая_работа.View.Pages.PageTask
+{
+    /// <summary>
+    /// Логика взаимодействия для Task8Page.xaml
+    /// </summary>
+    public partial class Task8Page : Page
+    {
+        public Task8Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
